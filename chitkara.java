912661902a20403b36afc3400c4245407ef358a3
AAROHI DIXIT <148526981+aarohidixit31@users.noreply.github.com>
2024-12-06 18:57:36 +0530

    Add files via upload

---
+++ chitkara.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class chitkara {
+    public static void main(String[] args)
+    {
+       Scanner input = new Scanner(System.in);
+       System.out.println(input.nextInt());
+    }
+}
